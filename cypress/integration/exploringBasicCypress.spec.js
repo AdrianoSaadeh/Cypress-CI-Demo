176d49e7c47bca82b518cb12bffe8281ecab9779
Adriano Omar Saadeh | SellersFunding <adriano.saadeh@sellersfunding.com>
2022-03-10 07:37:21 -0300

    advanced tests spec added

--- cypress/integration/exploringBasicCypress.spec.js
+++ cypress/integration/exploringBasicCypress.spec.js
@@ -167,5 +167,9 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.contains('Cypress Automation Testing').should('be.visible')
     })
 
+    it('testa a página da política de privavidade de forma independente', function() {
+        cy.visit('./src/privacy.html')
+        cy.contains('Talking About Testing').should('be.visible')
+    })
     
 })
